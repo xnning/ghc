85bc116a11d783b0001245d2ce99a44fd588f6c2
ningning <xnningxie@gmail.com>
2018-06-06 16:22:02 -0400

    clean up

--- compiler/types/Unify.hs
+++ compiler/types/Unify.hs
@@ -1352,7 +1352,6 @@ ty_co_match menv subst (TyVarTy tv1) co lkco rkco
     else Just $ extendVarEnv subst tv1' $
                 castCoercionKindI co (mkSymCo lkco) (mkSymCo rkco)
 
-  -- TODO: EraseEqCo
   | otherwise
   = Nothing
 
