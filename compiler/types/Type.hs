045fe6317b932c870e9e5bfcd95cf9059b90579a
ningning <xnningxie@gmail.com>
2018-07-23 00:52:01 -0400

    Fixes in Type, TyCoRep

--- compiler/types/Type.hs
+++ compiler/types/Type.hs
@@ -205,7 +205,7 @@ module Type (
         tidyTyVarOcc,
         tidyTopType,
         tidyKind,
-        tidyTyVarBinder, tidyTyVarBinders
+        tidyTyCoVarBinder, tidyTyCoVarBinders
     ) where
 
 #include "HsVersions.h"
